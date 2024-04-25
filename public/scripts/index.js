00e3155cf1dba90d5e264bafb5af998138122559
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-04-25 14:17:32 +0400

    use wordle api to get & validate words

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,69 @@
-console.log("test");
+const API_URL = "https://words.dev-apis.com";
+
+let wordOfTheDay = "";
+let currentMode = "daily"; // daily | random
+let isLoading = true;
+
+const modeSelectorFormEl = document.querySelector(".mode-selector-form");
+const currentWordEl = document.querySelector(".current-word");
+
+function render() {
+  isLoading
+    ? (currentWordEl.textContent = "Loading...")
+    : (currentWordEl.textContent = wordOfTheDay);
+}
+
+async function getWordOfTheDay(mode) {
+  const isRandom = mode === "random";
+  const url = `${API_URL}/word-of-the-day?random=${isRandom}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    wordOfTheDay = data.word;
+    isLoading = false;
+  } catch (e) {
+    console.error(e);
+  }
+
+  render();
+}
+
+async function validateWord(word) {
+  const url = `${API_URL}/validate-word`;
+
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ word }),
+    });
+    const data = await res.json();
+    return data.validWord;
+  } catch (e) {
+    console.error(e);
+  }
+
+  render();
+}
+
+function init(mode) {
+  isLoading = true;
+  render();
+  getWordOfTheDay(mode);
+}
+
+modeSelectorFormEl.mode.forEach((radio) =>
+  radio.addEventListener("change", function (e) {
+    const newMode = e.target.value;
+    if (newMode !== currentMode) {
+      currentMode = e.target.value;
+      init(newMode);
+    }
+  }),
+);
+
+init(currentMode);
+
+// check word validation
+validateWord("crane").then((result) => console.log(result)); // true
+validateWord("abcde").then((result) => console.log(result)); // false
